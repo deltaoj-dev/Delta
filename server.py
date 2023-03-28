822ca183e568a8214fd1e8d4df67f5f8938da1aa
Eznibuil <77226416+liubinze@users.noreply.github.com>
2023-03-28 14:09:29 +0800

    bootstrap test

--- server.py
+++ server.py
@@ -8,6 +8,26 @@ class DeltaHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 			self.send_header("Content-type","text/html")
 			self.end_headers()
 			shutil.copyfileobj(open("index.html","rb"),self.wfile)
+		elif self.path=='/bootstrap.min.css':
+			self.send_response(http.server.HTTPStatus.OK)
+			self.send_header("Content-type","text/css")
+			self.end_headers()
+			shutil.copyfileobj(open("bootstrap.min.css","rb"),self.wfile)
+		elif self.path=='/bootstrap.min.css.map':
+			self.send_response(http.server.HTTPStatus.OK)
+			self.send_header("Content-type","application/json")
+			self.end_headers()
+			shutil.copyfileobj(open("bootstrap.min.css.map","rb"),self.wfile)
+		elif self.path=='/bootstrap.bundle.min.js':
+			self.send_response(http.server.HTTPStatus.OK)
+			self.send_header("Content-type","application/x-javascript")
+			self.end_headers()
+			shutil.copyfileobj(open("bootstrap.bundle.min.js","rb"),self.wfile)
+		elif self.path=='/bootstrap.bundle.min.js.map':
+			self.send_response(http.server.HTTPStatus.OK)
+			self.send_header("Content-type","application/json")
+			self.end_headers()
+			shutil.copyfileobj(open("bootstrap.bundle.min.js.map","rb"),self.wfile)
 		else:
 			self.send_error(http.server.HTTPStatus.NOT_FOUND)
 
