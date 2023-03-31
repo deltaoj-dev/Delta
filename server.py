11de9908d74788b32927678e2560f91e93b93b9f
Eznibuil <77226416+liubinze@users.noreply.github.com>
2023-03-31 23:29:34 +0800

    Cookie test

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
+import http.cookies
 import http.server
 import shutil
 
-class DeltaCookie(http.cookie.BaseCookie):
+class DeltaCookie(http.cookies.BaseCookie):
 	def value_decode(self,val):
 		return val,val
 	def value_encode(self,val):
@@ -10,16 +11,10 @@ class DeltaCookie(http.cookie.BaseCookie):
 
 class DeltaHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 	def do_GET(self):
-		if self.path=='/':
-			self.send_response(http.server.HTTPStatus.OK)
-			self.send_header("Content-type","text/html;char-set=utf-8")
-			self.end_headers()
-			shutil.copyfileobj(open("index.html","rb"),self.wfile)
-		elif self.path=='/favicon.ico':
+		if self.path=='/favicon.ico':
+			f=open(self.path[1:],"rb")
 			self.send_response(http.server.HTTPStatus.OK)
 			self.send_header("Content-type","image/x-icon")
-			self.end_headers()
-			shutil.copyfileobj(open(self.path[1:],"rb"),self.wfile)
 		elif self.path[:8]=='/assets/':
 			try:
 				f=open(self.path[1:],"rb")
@@ -30,12 +25,19 @@ class DeltaHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 					self.send_header("Content-type","application/x-javascript")
 				elif self.path[-4:]=='.map' or self.path[-5:]=='.json':
 					self.send_header("Content-type","application/json")
-				self.end_headers()
-				shutil.copyfileobj(f,self.wfile)
 			except IOError:
 				self.send_error(http.server.HTTPStatus.NOT_FOUND)
+				return
+		elif self.path=='/':
+			f=open("index.html","rb")
+			self.send_response(http.server.HTTPStatus.OK)
+			self.send_header("Content-type","text/html;char-set=utf-8")
+			self.send_header("Set-Cookie","Delta=test;Max-age=604800")
 		else:
 			self.send_error(http.server.HTTPStatus.FORBIDDEN)
+			return
+		self.end_headers()
+		shutil.copyfileobj(f,self.wfile)
 
 if __name__=='__main__':
 	server_address=('',8000)
